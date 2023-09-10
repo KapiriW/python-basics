25e43356cbb65dcc52d283c29559c7dabf3f75ff
KapiriW <143568240+KapiriW@users.noreply.github.com>
2023-09-11 09:11:44 +1200

    Add files via upload

---
+++ perimeter.py
@@ -0,0 +1,12 @@
+
+valid = False
+while not valid:
+  response = float(input("Enter a number: "))
+if response> 0:
+
+else:
+
+print("Thank you, you chose {response}")
+valid = True
+print("please enter a number that is higher than zero")
+print()
